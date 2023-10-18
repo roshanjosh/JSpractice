523efa5e054bf88f630557965305cc72d8ee9606
Joseph Rosh <josephrosh12@icloud.com>
2023-10-18 23:32:43 +0200

    Working with Modal tuts by Ninja modified:   src/App.vue modified:   src/components/HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> {{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <HelloWorld theme="" @close = "toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway</h1>
+      <p>Get yours today, enter the game to win a logitec mouse and keyboard</p>
+    </HelloWorld>
+  </div>
+  <button @click.alt="toggleModal">Open Modal (alt)</button>
 </template>
 
 <script>
@@ -10,7 +21,22 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      title: 'My first Vue app :)',
+
+      showModal: false
+    }
+  },
+
+  methods: {
+      toggleModal() {
+        this.showModal = !this.showModal
+      }
+    }
+
+
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale'}">
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+
+    </div>
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  border-radius: 100px;
+  background: white;
+  border-radius: 10px;
 }
-ul {
-  list-style-type: none;
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.modal h1 {
+  color: #03cfb4;
+  border: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  .modal p {
+    font-style: normal;
+  }
+
+  .modal.sale {
+    color: white;
+  }
+
+  .modal.sale h1 {
+    color: white;
+  }
+
+  .modal.sale.actions {
+    color: white;
+  }
+
+  .modal.sale.actions a {
+    color: white;
+  }
+ 
+  
 </style>
